8448e569ff8a592ae58b2b4f8b951d6a0be9f40e
Preflower <preflower.lin@gmail.com>
2022-03-18 18:07:17 +0800

    fix(toast): toast style not horizontal spacing error

---
+++ .changeset/cool-glasses-remain.md
@@ -0,0 +1,6 @@
+---
+"@react-native-web-adapter/example": patch
+"react-native-web-simple-toast": patch
+---
+
+update toast style that add horizontal spacing
--- example/webpack.config.js
+++ example/webpack.config.js
@@ -9,6 +9,7 @@ const { presets } = require(`${appDirectory}/babel.config.js`)
 const compileNodeModules = [
   // Add every react-native package that needs compiling
   // 'react-native-gesture-handler',
+  'react-native-web-simple-toast'
 ].map((moduleName) => path.resolve(appDirectory, `node_modules/${moduleName}`))
 
 const babelLoaderConfiguration = {
